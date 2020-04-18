4bb6fef5022a13257ad0c41184605279965a96f4
makubob <makupi@icloud.com>
2020-04-18 21:18:33 +0200

    Adds villager name search if there is no exact match

--- acnh/cogs/nookipedia.py
+++ acnh/cogs/nookipedia.py
@@ -1,3 +1,5 @@
+import difflib
+
 import aiohttp
 import discord
 from discord.ext import commands
@@ -7,6 +9,10 @@ from acnh.utils import config, create_embed
 # match to list of villagers with difflib.get_close_matches
 
 VILLAGER_API = "https://nookipedia.com/api/villager/{name}/?api_key={api_key}"
+VILLAGER_CATEGORY_LIST = (
+    "https://nookipedia.com/w/api.php?action=query&list=categorymembers&&cmlimit=max&cmtitle"
+    "=Category:Villagers&format=json"
+)
 
 
 async def fetch_json(url):
@@ -19,20 +25,34 @@ async def fetch_villager(name, api_key):
     return await fetch_json(VILLAGER_API.format(name=name, api_key=api_key))
 
 
+async def query_villager_list() -> list:
+    r = await fetch_json(VILLAGER_CATEGORY_LIST)
+    member_list = r.get("query").get("categorymembers")
+    villagers = []
+    for member in member_list:
+        name = member.get("title")
+        if "Category" in name or "islander" in name:
+            continue
+        villagers.append(name)
+    return villagers
+
+
 class Nookipedia(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.api_key = config.nookipedia_key
-
-    async def query_villager_list(self) -> list:
-        pass
+        self.villagers = []
 
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"{type(self).__name__} Cog ready.")
+        self.villagers = await query_villager_list()
 
     @commands.command()
     async def villager(self, ctx, *, name: str):
+        name, msg = await self.find_villager(ctx, name)
+        if name is None:
+            return
         result = await fetch_villager(name, self.api_key)
         quote = result.get("quote", "")
         image = result.get("image", None)
@@ -56,7 +76,53 @@ class Nookipedia(commands.Cog):
             embed.add_field(name="Phrase", value=phrase)
         if birthday:
             embed.add_field(name="Birthday", value=f"{birthday} ({sign})")
-        await ctx.send(embed=embed)
+        if msg is None:
+            await ctx.send(embed=embed)
+        else:
+            await msg.edit(embed=embed)
+
+    async def find_villager(self, ctx, name):
+        # check exact match
+        if name.capitalize() in self.villagers:
+            return name.capitalize(), None
+        tmp = f"{name.capitalize()} (villager)"
+        if tmp in self.villagers:
+            return tmp, None
+        matches = difflib.get_close_matches(name, self.villagers, n=6)
+        print(matches)
+        embed = await create_embed()
+        if len(matches) == 1:
+            return matches[0], None
+        elif len(matches) == 0:
+            embed.description = "No matching villager found!"
+            await ctx.send(embed=embed)
+            return None, None
+        else:
+            choices = {}
+            for index, match in enumerate(matches):
+                choices[f"{index}⃣"] = match
+            for k, v in choices.items():
+                embed.add_field(name=k, value=v, inline=True)
+            embed.set_footer(text="React to choose one villager!")
+            msg = await ctx.send(embed=embed)
+            for emoji in choices.keys():
+                await msg.add_reaction(emoji)
+
+            def check(_reaction, _user):
+                return (
+                    _reaction.message.id == msg.id
+                    and _user.id == ctx.author.id
+                    and _reaction.emoji in choices.keys()
+                )
+
+            reaction, user = await self.bot.wait_for(
+                "reaction_add", check=check, timeout=300.0
+            )
+            try:
+                await msg.clear_reactions()
+            except discord.Forbidden:
+                pass
+            return choices.get(reaction.emoji), msg
 
 
 def setup(bot):

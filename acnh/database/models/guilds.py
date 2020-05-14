e61bde2ae8e8b360bbbf089eb702fd29f4d869f0
makubob <makupi@icloud.com>
2020-05-14 22:39:08 +0200

    Adds guild_id foreignkey

--- acnh/database/models/guilds.py
+++ acnh/database/models/guilds.py
@@ -2,7 +2,8 @@ from acnh.database import db
 
 
 class Guild(db.Model):
-    __tablename__ = 'guilds'
+    __tablename__ = "guilds"
 
     id = db.Column(db.BIGINT, primary_key=True)
     prefix = db.Column(db.String)
+    local_turnips = db.Column(db.Boolean, default=False)

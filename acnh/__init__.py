fe5fd74f009f7be7cd5ebf2587379dafa36d15c9
makubob <makupi@icloud.com>
2020-05-15 20:11:16 +0200

    Version bump

--- acnh/__init__.py
+++ acnh/__init__.py
@@ -8,7 +8,7 @@ from acnh.database.models import Guild
 from acnh.utils import config, get_guild_prefix
 from discord.ext import commands
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 invite_link = "https://discordapp.com/api/oauth2/authorize?client_id={}&scope=bot&permissions=8192"
 
